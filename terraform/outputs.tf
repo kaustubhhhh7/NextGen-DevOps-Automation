15769c08d2ec5b92dc6796c673c9d8e81dd2200b
kaustubhhhh7 <kaustubh7205@gmail.com>
2026-01-14 23:36:42 +0530

    feat(iac): output ECR repository URL

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,3 +14,8 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.main.id
 }
+
+output "ecr_repository_url" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.app_repo.repository_url
+}
